aea99e78d2d780b84eaf4462d6d2498b002c96bf
KristinaNikolic99 <krisnikolic992@gmail.com>
2025-04-24 20:19:40 +0200

    Cart and Shop features resolved

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -200,6 +200,22 @@ const addProductReview = asyncHandler(async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(400).json(error.message);
+    };
+  });
+
+  const filterProducts = asyncHandler(async (req, res) => {
+    try {
+        const {checked, radio} = req.body;
+
+        let args = {};
+        if(checked.length > 0) args.category = checked;
+        if(radio.length) args.price = {$gte: radio[0], $lte: radio[1]};
+
+        const products = await Product.find(args);
+        res.json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "Server Error"})
     }
   })
 
@@ -212,5 +228,6 @@ export {
     fetchAllProducts,
     addProductReview,
     fetchTopProducts,
-    fetchNewProducts
+    fetchNewProducts,
+    filterProducts
 };
--- frontend/src/redux/features/store.js
+++ frontend/src/redux/features/store.js
@@ -3,7 +3,9 @@ import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "../api/apiSlice";
 import authReducer from '../features/auth/authSlice';
 import favoritesReducer from '../features/favorites/favoriteSlice.js'
+import cartSliceReducer from '../features/cart/cartSlice.js'
 import {getFavoritesFromLocalStorage} from '../../Utils/localStorage.js'
+import shopReducer from '../../redux/features/shop/shopSlice.js'
 
 const initialFavorites = getFavoritesFromLocalStorage() || [];
 
@@ -12,6 +14,8 @@ const store = configureStore({
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authReducer,
         favorites: favoritesReducer,
+        cart: cartSliceReducer,
+        shop: shopReducer,
     },
 
     preloadedState: {
